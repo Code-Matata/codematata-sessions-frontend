25344c6ab7b88b6976bc4323673e3bc04bc295ba
kibuikaCodes <kibuikasteve@gmail.com>
2021-05-14 10:57:27 +0300

    :sparkles: add hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { colors } from "../resources/ThemeColors";
+import CodeImage from "../resources/code-1.jpg";
+import TypeEffect from "./TypeEffect";
+import SearchCard from "../components/SearchComponent";
+
+function HeroSection() {
+  return (
+    <MainDiv>
+      <TopDiv>
+        <HeroTextDiv>
+          <HeroText>Made for those who &nbsp;</HeroText>
+          <TypeEffect />
+        </HeroTextDiv>
+        <SearchCard />
+      </TopDiv>
+    </MainDiv>
+  );
+}
+
+export default HeroSection;
+
+const MainDiv = styled.div`
+  height: auto;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 3em;
+  background-color: ${colors.main};
+  z-index: -1;
+`;
+
+const TopDiv = styled.div`
+  height: 50vh;
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: space-evenly;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.6)),
+    url(${CodeImage});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  z-index: 0;
+`;
+
+const HeroTextDiv = styled.div`
+  display: flex;
+`;
+
+const HeroText = styled.p`
+  color: ${colors.white};
+  font-weight: 400;
+`;
---
+++ src/resources/ThemeColors.js
@@ -0,0 +1,7 @@
+export const colors = {
+  mainColor: "#3e4480",
+  darkMainColor: "#262a52",
+  gray: "#d8daeb",
+  white: "#ffffff",
+  black: "#000000",
+};
