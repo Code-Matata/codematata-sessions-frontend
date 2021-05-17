52955c68b3595ce70d148277db0d3c9c869c0ef9
kibuikaCodes <kibuikasteve@gmail.com>
2021-05-17 14:11:00 +0300

    :sparkles: add loader

---
+++ src/pages/LoaderPage/LoaderPage.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Loader from "react-loader-spinner";
+import styled from "styled-components";
+
+const timeoutSeconds = 3000; //3 secs
+
+class LoaderPage extends Component {
+  state = { redirect: false };
+
+  componentDidMount() {
+    this.id = setTimeout(
+      () => this.setState({ redirect: true }),
+      timeoutSeconds
+    );
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.id);
+  }
+  render() {
+    return this.state.redirect ? (
+      <Redirect to="/dash" />
+    ) : (
+      <MainDiv>
+        <Loader
+          type="ThreeDots"
+          color="#262a52"
+          height={100}
+          width={100}
+          timeout={timeoutSeconds}
+        />
+      </MainDiv>
+    );
+  }
+}
+
+export default LoaderPage;
+
+const MainDiv = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+`;
