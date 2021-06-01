09b0b53dd8289dc870bc31ef4cd701a420358d8f
kibuikaCodes <kibuikasteve@gmail.com>
2021-06-01 14:59:01 +0300

    :bug: fix bug in oauth2

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_BASE_URL;
+export const API_BASE_URL = "https://code-matata.herokuapp.com";
 export const ACCESS_TOKEN = "accessToken";
 
 export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
