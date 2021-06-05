2dd96cb45ef437335603b22c302fe7e22ec662b1
kibuikaCodes <kibuikasteve@gmail.com>
2021-06-05 23:51:10 +0300

    log token

--- src/pages/SessionPage/index.js
+++ src/pages/SessionPage/index.js
@@ -10,7 +10,7 @@ class SessionPage extends Component {
 
   componentDidMount() {
     window.scrollTo(0, 0);
-    console.log("Access token", localStorage.getItem(ACCESS_TOKEN));
+    console.log("Access token", localStorage.getItem("ACCESS_TOKEN"));
   }
   render() {
     return (
