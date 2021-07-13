3eaca4864a4ec06dc4cb1e4ae8d1bf7a46d9f6d1
Steve Kibuika <kibuikasteve@gmail.com>
2021-07-13 15:49:57 +0300

    test custom widget

--- src/pages/SessionPage/Eventt.js
+++ src/pages/SessionPage/Eventt.js
@@ -5,36 +5,26 @@ import { colors } from "../../resources/ThemeColors";
 export default function Eventt({ eventId }) {
   return (
     <MainDiv>
-      <noscript></noscript>
-
-      <a
-        href={`https://www.eventbrite.com/e/html-css-for-beginners-tickets-${eventId}`}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <Button id="eventbrite-widget-modal-trigger-163175292693" type="button">
-          Get your FREE ticket here.
-        </Button>
-      </a>
-
-      <noscript>Buy Tickets on Eventbrite</noscript>
+      <div id="eventbrite-widget-container-163175292693"></div>
 
       <script src="https://www.eventbrite.com/static/widgets/eb_widgets.js"></script>
 
       <script
         dangerouslySetInnerHTML={{
           __html: `
-            var exampleCallback = function() {
+    var exampleCallback = function() {
         console.log('Order complete!');
     };
 
     window.EBWidgets.createWidget({
         widgetType: 'checkout',
         eventId: '163175292693',
-        modal: true,
-        modalTriggerElementId: 'eventbrite-widget-modal-trigger-163175292693',
-        onOrderComplete: exampleCallback
+        iframeContainerId: 'eventbrite-widget-container-163175292693',
+
+        iframeContainerHeight: 425,  
+        onOrderComplete: exampleCallback  
     });
+
         `,
         }}
       />
